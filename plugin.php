ef1ea7d9fa6fb63791bfc74882230c11d3bc9109
Olivia Kaufmann <olivia.kaufmann@ilub.unibe.ch>
2020-05-26 13:14:15 +0200

    plugin update

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'usrto';
 $version = '2.1.0';
 $ilias_min_version = '5.3.0';
-$ilias_max_version = '5.4.999';
+$ilias_max_version = '6.0.999';
 $responsible = 'studer + raimann ag - Team Core';
 $responsible_mail = 'support-core@studer-raimann.ch';
