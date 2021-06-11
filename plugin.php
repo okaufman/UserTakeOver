7b308223e051aadafa6cad36221882185e704799
Olivia Kaufmann <olivia.kaufmann@ilub.unibe.ch>
2021-06-11 11:17:27 +0200

    Update for ilias7

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 $id                = 'usrto';
 $version           = '3.0.3';
 $ilias_min_version = '6.0';
-$ilias_max_version = '6.9999';
+$ilias_max_version = '7.9999';
 $responsible       = 'studer + raimann ag - Team Core 1';
 $responsible_mail  = 'support-core1@studer-raimann.ch';
 
